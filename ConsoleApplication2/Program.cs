58900539c7da23979ecba33c16ada6deecb762d2
kovacsmate97 <kovimatyi8@freemail.hu>
2017-02-23 12:50:29 +0100

    asd

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,6 +10,14 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
+            C c = new C();
+            //S1,S2,S5,V2,S6,S7
+            // c.S1(); //A::S1 A::V1 A::S3
+            // c.S2(); //A::S2 A::S1 A::V1 A::S3
+            // c.S5(); //B::S5 B::S3
+            // 
+            c.S5();
+            Console.ReadKey();
         }
     }
 }
