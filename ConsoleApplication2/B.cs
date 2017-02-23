1612c6cda80439eee67eb30e7da3deff6725ec96
kovacsmate97 <kovimatyi8@freemail.hu>
2017-02-23 12:34:59 +0100

    asd

---
+++ ConsoleApplication2/B.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    public class B : A
+    {
+        protected void S4()
+        {
+            Console.WriteLine("B::S4");
+            V1();
+        }
+
+        public void S5()
+        {
+            Console.WriteLine("B::S5");
+            S3();
+        }
+
+        protected void S3()
+        {
+            Console.WriteLine("B::S3");
+
+        }
+
+        public virtual void V2()
+        {
+            Console.WriteLine("B::V2");
+            V1();
+        }
+    }
+}
---
+++ ConsoleApplication2/D.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    public class D : B
+    {
+        public void S7()
+        {
+            Console.WriteLine("D::S7");
+            S3();
+        }
+
+        public void S8()
+        {
+            Console.WriteLine("D::S7");
+            V2();
+        }
+    }
+}
